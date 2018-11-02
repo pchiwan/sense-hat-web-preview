afe0013a524130f0ab903f3773dbdd3c4998c885
Sílvia Mur Blanch <pchiwan@gmail.com>
2018-11-02 12:21:55 +0100

    Use nodemon for webemu CLI

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const nodeCLI = require('shelljs-nodecli')
+const nodecli = require('shelljs-nodecli')
 
 const webEmuPath = `${path.join(__dirname, '..', 'dist')}/index.js`
 const entryFileName = process.argv[process.argv.length - 1]
 const entryFilePath = `${path.join(process.cwd(), entryFileName)}`
+const nodemonPath = `${path.join(process.cwd(), 'node_modules', '.bin')}/nodemon`
 
-nodeCLI.exec('node', webEmuPath, entryFilePath)
+nodecli.exec(nodemonPath, webEmuPath, entryFilePath)

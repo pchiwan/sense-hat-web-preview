eecf8fc5d8d24b5318c8767f431efa4f0a05d2c3
Sílvia Mur <pchiwan@gmail.com>
2018-11-03 00:03:46 +0100

    Add rest of pending Sense HAT LED methods

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,13 @@ const { MATRIX_SIZE, BLACK, WHITE } = require('./constants')
 const TEXT_ASSETS = 'sense_hat_text'
 const LETTER_PIXELS = 5 * 8
 
+const PIXEL_MAP = {
+  0: (x, y) => y * MATRIX_SIZE + x,
+  90: (x, y) => y + ((MATRIX_SIZE - 1) - x) * MATRIX_SIZE,
+  180: (x, y) => ((MATRIX_SIZE - 1) - y) * MATRIX_SIZE + ((MATRIX_SIZE - 1) - x),
+  270: (x, y) => (MATRIX_SIZE - 1) - y + x * MATRIX_SIZE
+}
+
 function isValidColorValue (value) {
   return value >= 0 && value <= 255
 }
@@ -22,11 +29,13 @@ function rgbArray (r = 0, g = 0, b = 0) {
 
 function pngTopixels (png) {
   return Array.from(new Array(png.width * png.height), (_, i) => {
-    return Array.from(new Array(3), (_, j) => png.data[i * 4 + j]);
+    return Array.from(new Array(3), (_, j) => png.data[i * 4 + j])
   })
 }
 
-function loadImageSync(filePath) {
+// Accepts a path to an 8 x 8 image file and updates the LED matrix with
+// the image
+function loadImageSync (filePath) {
   try {
     fs.accessSync(filePath)
   } catch (error) {
@@ -40,8 +49,37 @@ function loadImageSync(filePath) {
   return pixels
 }
 
+function loadImageAsync (filePath) {
+  return new Promise((resolve, reject) => {
+    fs.access(filePath, readFile)
+
+    function readFile(error) {
+      if (error) {
+        return reject(`${filePath} not found`)
+      }
+      fs.readFile(filePath, parsePNG)
+    }
+
+    function parsePNG(error, buf) {
+      if (error) {
+        return reject(`${filePath} could not be read`)
+      }
+      new PNG().parse(buf, convertPNG)
+    }
+
+    function convertPNG(error, png) {
+      if (error) {
+        return reject(`Could not parse PNG ${error.message}`)
+      }
+
+      const pixels = pngTopixels(png)
+      resolve(pixels)
+    }
+  })
+}
+
 // Builds a character indexed object of pixels used by show_message
-function loadTextAssets(textImageFile, textFile) {
+function loadTextAssets (textImageFile, textFile) {
   const textPixels = loadImageSync(textImageFile, false)
   const loadedText = fs.readFileSync(textFile, 'utf8')
 
@@ -67,20 +105,112 @@ function isBlack (color) {
   return color.every(value => value === 0)
 }
 
-function getCharPixels(character, letters) {
+function getCharPixels (character) {
   return character.length === 1 && character in letters
     ? letters[character]
     : letters['?']
 }
 
-function letterPixels(letter, textColor = WHITE, backColor = BLACK) {
+function letterPixels (letter, textColor = WHITE, backColor = BLACK) {
+  const letterPadding = Array(MATRIX_SIZE * 2).fill(backColor)
+
   return Array(MATRIX_SIZE).fill(backColor)
-    .concat(getCharPixels(letter).map(rgb => isBlack(rgb) ? backColor : textColor))
-    .concat(Array(MATRIX_SIZE * 2).fill(backColor))
+    .concat(getCharPixels(letter).map(
+      rgb => isBlack(rgb) ? backColor : textColor)
+    )
+    .concat(letterPadding)
+}
+
+function scrollPixels (message, textColor = WHITE, backColor = BLACK) {
+  const stringPadding = Array(MATRIX_SIZE).fill(backColor)
+  const letterPadding = Array(MATRIX_SIZE * 2).fill(backColor)
+
+  return message.split('')
+    .reduce((pixels, char) => pixels
+      .concat(trimWhitespace(getCharPixels(char)).map(
+        rgb => isBlack(rgb) ? backColor : textColor)
+      )
+      .concat(letterPadding), stringPadding)
+    .concat(stringPadding)
+}
+
+function trimBack (pixels) {
+  return pixels.slice(-8).every(isBlack)
+    ? trimBack(pixels.slice(0, -8))
+    : pixels
+}
+
+function trimFront (pixels) {
+  return pixels.slice(0, 8).every(isBlack)
+    ? trimFront(pixels.slice(8))
+    : pixels
+}
+
+function trimWhitespace (pixels) {
+  return !pixels.every(isBlack)
+    ? trimBack(trimFront(pixels))
+    : pixels
+}
+
+function checkXY (x, y) {
+  if (x < 0 || x > MATRIX_SIZE - 1) {
+    throw Error(`x=${x} violates 0 <= x <= ${MATRIX_SIZE - 1}`)
+  }
+  if (y < 0 || y > MATRIX_SIZE - 1) {
+    throw Error(`y=${y} violates 0 <= y <= ${MATRIX_SIZE - 1}`)
+  }
+}
+
+function getCoord (x, y, rotation) {
+  return PIXEL_MAP[rotation](x, y)
+}
+
+function mirrorH(pixels, width) {
+  return pixels.length
+    ? pixels
+      .slice(0, width)
+      .reverse()
+      .concat(mirrorH(pixels.slice(width), width))
+    : []
+}
+
+function horizontalMirror (pixels) {
+  return mirrorH(pixels, Math.sqrt(pixels.length))
+}
+
+function mirrorV (pixels, width) {
+  return pixels.length
+    ? pixels
+      .slice(pixels.length - width)
+      .concat(mirrorV(pixels.slice(0, pixels.length - width), width))
+    : []
+}
+
+function verticalMirror (pixels) {
+  return mirrorV(pixels, Math.sqrt(pixels.length))
+}
+
+function sleep (timeout) {
+  return new Promise((resolve) => setTimeout(resolve, timeout * 1000000))
+}
+
+function syncCurriedFunction (sync, func) {
+  return (...args) => func.apply(this, [sync, ...[].slice.call(args)])
 }
 
 module.exports = {
-  letters,
+  checkXY,
+  getCharPixels,
+  getCoord,
+  horizontalMirror,
+  isBlack,
   letterPixels,
-  rgbArray
+  loadImageAsync,
+  loadImageSync,
+  rgbArray,
+  scrollPixels,
+  sleep,
+  syncCurriedFunction,
+  trimWhitespace,
+  verticalMirror
 }
--- src/webLeds.js
+++ src/webLeds.js
@@ -1,12 +1,54 @@
-const { letterPixels, rgbArray } = require('./utils')
-const { MATRIX_LENGTH } = require('./constants')
+const {
+  checkXY,
+  getCoord,
+  horizontalMirror,
+  letterPixels,
+  loadImageAsync,
+  loadImageSync,
+  rgbArray,
+  scrollPixels,
+  sleep,
+  syncCurriedFunction,
+  verticalMirror
+} = require('./utils')
+const {
+  BLACK,
+  MATRIX_LENGTH,
+  MATRIX_SIZE
+} = require('./constants')
+
+const ONE_SECOND_IN_MS = 1000
+const DEFAULT_SCROLL_SPEED = 0.1
+const DEFAULT_FLASH_SPEED = 0.5
+
+const noop = () => {}
 
 const WebLeds = socket => {
-  let innerMatrix
-  let rotation
+  let innerMatrix = Array(MATRIX_LENGTH).fill(BLACK)
+  let rotation = 0
 
   const paint = () => {
-    socket.emit('updateMatrix', innerMatrix)
+    socket.emit('updateMatrix', rotateMatrix(innerMatrix, rotation))
+  }
+
+  const rotateMatrix = (matrix, rotation) => {
+    const projectedMatrix = []
+    for (let y = 0; y < MATRIX_SIZE; y++) {
+      for (let x = 0; x < MATRIX_SIZE; x++) {
+        projectedMatrix.push(matrix[getCoord(x, y, rotation)])
+      }
+    }
+    return projectedMatrix
+  }
+
+  const setMatrixAndPaint = matrix => {
+    innerMatrix = matrix
+    paint()
+  }
+
+  const setPixelAndPaint = (x, y, rgb) => {
+    innerMatrix[getCoord(x, y, rotation)] = rgb
+    paint()
   }
 
   const checkAngle = (angle = 0) => {
@@ -18,47 +60,86 @@ const WebLeds = socket => {
     return angle
   }
 
-  const clear = (r, g, b, callback = () => {}) => {
-    setPixels(Array(MATRIX_LENGTH).fill(rgbArray(r, g, b)), callback)
+  const clear = (sync = true, r, g, b, callback = noop) => {
+    setPixels(sync, Array(MATRIX_LENGTH).fill(rgbArray(r, g, b)), callback)
   }
 
-  const getPixels = (callback = () => {}) => {
-    callback(null, innerMatrix)
-    return innerMatrix
+  const getPixels = (sync = true, callback = noop) => {
+    return sync
+      ? innerMatrix
+      : callback(null, innerMatrix)
   }
 
-  const setPixels = (matrix, callback = () => {}) => {
+  const getPixel = (sync = true, x, y, callback = noop) => {
+    try {
+      checkXY(x, y)
+    } catch (error) {
+      return sync
+        ? console.error(error.message)
+        : callback(error)
+    }
+  
+    return sync
+      ? innerMatrix[getCoord(x, y, rotation)]
+      : callback(innerMatrix[getCoord(x, y, rotation)])
+  }
+
+  const setPixels = (sync = true, matrix, callback = noop) => {
     if (matrix.length !== MATRIX_LENGTH) {
-      console.error(`Pixel arrays must have ${MATRIX_LENGTH} elements`)
+      const errorMessage = `Pixel arrays must have ${MATRIX_LENGTH} elements`
+      return sync
+        ?  console.error(errorMessage)
+        : callback(Error(errorMessage))
+    }
+    
+    return sync
+      ? setMatrixAndPaint(matrix)
+      : new Promise(resolve => {
+        resolve()
+        setMatrixAndPaint(matrix)
+        callback(null, innerMatrix)
+      })
+  }
+
+  const setPixel = (sync = true, x, y, r, g, b, callback = noop) => {
+    const rgb = rgbArray(r, g, b)
+
+    try {
+      checkXY(x, y)
+    } catch (error) {
+      return sync
+        ? console.error(error.messsage)
+        : callback(error)
     }
 
-    innerMatrix = matrix
-    paint()
-    callback(null, innerMatrix)
-  }
-
-  const setPixel = () => {
-    console.log('work in progress')
+    return sync
+      ? setPixelAndPaint(x, y, rgb)
+      : new Promise(resolve => {
+        resolve()
+        setPixelAndPaint(x, y, rgb)
+        callback(null, innerMatrix)
+      })
   }
 
   // Sets the LED matrix rotation for viewing, adjust if the Pi is upside
   // down or sideways. 0 is with the Pi HDMI port facing downwards
-  const setRotation = (r, redraw = true, callback = () => {}) => {
-    rotation = checkAngle(r)
+  const setRotation = (sync = true, angle, redraw = true, callback = noop) => {
+    rotation = checkAngle(angle)
 
-    if (!redraw) return callback(null)
+    if (!redraw) {
+      return callback(null)
+    }
 
-    getPixels((error, pixels) => {
-      if (error) return callback(error)
-      setPixels(pixels, callback)
-    })
+    setPixels(sync, innerMatrix, callback)
   }
 
   // Displays a single text character on the LED matrix using the specified colors
-  const showLetter = (letter, textColor, backColor, callback = () => {}) => {
+  const showLetter = (sync = true, letter, textColor, backColor, callback = noop) => {
     if (letter.length !== 1) {
-      callback(Error('Only one character may be passed into showLetter'))
-      return
+      const errorMessage = 'Only one character may be passed into showLetter'
+      return sync
+        ? console.error(errorMessage)
+        : callback(Error(errorMessage))
     }
 
     const pixels = letterPixels(letter, textColor, backColor)
@@ -67,33 +148,181 @@ const WebLeds = socket => {
     // text, see loadTextAssets
     const previousRotation = rotation
     rotation = (rotation + 90) % 360
-    setPixels(pixels, (error) => {
+
+    if (sync) {
+      setPixels(true, innerMatrix)
       rotation = previousRotation
-      callback(error)
+    } else {
+      setPixels(false, pixels, (error) => {
+        rotation = previousRotation
+        callback(error)
+      })
+    }
+  }
+
+  // Scrolls a string of text across the LED matrix using the specified
+  // speed and colors
+  const showMessage = (
+    sync = true,
+    textString,
+    scrollSpeed = DEFAULT_SCROLL_SPEED,
+    textColor,
+    backColor,
+    callback = noop
+  ) => {
+    const pixels = scrollPixels(textString, textColor, backColor)
+
+    // We must rotate the pixel map left through 90 degrees when drawing
+    // text, see loadTextAssets
+    const previousRotation = rotation
+    rotation = (rotation + 90) % 360
+
+    const scrollSync = pixels => {
+      if (pixels.length < MATRIX_LENGTH) return
+      setPixels(true, pixels.slice(0, MATRIX_LENGTH))
+      sleep(scrollSpeed)
+      scroll(pixels.slice(MATRIX_SIZE))
+    }
+
+    const scroll = pixels => {
+      if (pixels.length > MATRIX_LENGTH) {
+        setPixels(pixels.slice(0, MATRIX_LENGTH), error => {
+          if (error) {
+            rotation = previousRotation
+            return callback(error)
+          }
+          setTimeout(scroll, scrollSpeed * ONE_SECOND_IN_MS, pixels.slice(MATRIX_SIZE))
+        })
+      } else {
+        rotation = previousRotation
+        return callback(null)
+      }
+    }
+
+    if (sync) {
+      scrollSync(pixels)
+      rotation = previousRotation
+    } else {
+      scroll(pixels)
+    }
+  }
+
+  const flashMessage = (
+    sync = true,
+    message,
+    flashSpeed = DEFAULT_FLASH_SPEED,
+    textColor,
+    backColor,
+    callback = noop
+  ) => {
+    const flash = message => {
+      if (message.length) {
+        showLetter(message[0], textColor, backColor, (error) => {
+          if (error) {
+            return console.error(error.message)
+          }
+          setTimeout(flash, flashSpeed * ONE_SECOND_IN_MS, message.slice(1))
+        })
+      } else {
+        return callback(null)
+      }
+    }
+
+    if (sync) {
+      message.split('').forEach(char => {
+        showLetter(true, char, textColor, backColor)
+        sleep(flashSpeed)
+      })
+    } else {
+      flash(message)
+    }
+  }
+
+  // Flip LED matrix horizontal
+  const flipH = (sync = true, redraw = true, callback = noop) => {
+    const flipped = horizontalMirror(innerMatrix)
+    
+    if (redraw) {
+      return setPixels(sync, flipped, callback)
+    }
+    
+    return sync
+      ? flipped
+      : callback(null, flipped)
+  }
+
+  // Flip LED matrix vertical
+  const flipV = (sync = true, redraw = true, callback = noop) => {
+    const flipped = verticalMirror(innerMatrix)
+
+    if (redraw) {
+      return setPixels(sync, flipped, callback)
+    }
+
+    return sync
+      ? flipped
+      : callback(null, flipped)
+  }
+
+  const loadImage = (sync = true, filePath, redraw = true, callback = noop) => {
+    if (sync) {
+      const pixels = loadImageSync(filePath)
+      if (redraw) {
+        setPixels(true, pixels)
+      }
+      return pixels
+    }
+
+    loadImageAsync(filePath).then(pixels => {
+      if (redraw) {
+        setPixels(false, pixels, callback)
+      } else {
+        callback(null, pixels)
+      }
     })
   }
 
-  const showMessage = () => {
-    console.log('work in progress')
-  }
-
-  const flashMessage = () => {
-    console.log('work in progress')
-  }
-
   return {
-    clear,
-    getPixels,
-    setPixels,
-    setPixel,
-    setRotation,
-    showLetter,
-    showMessage,
-    flashMessage,
+    clear: syncCurriedFunction(false, clear),
+    setPixel: syncCurriedFunction(false, setPixel),
+    getPixel: syncCurriedFunction(false, getPixel),
+    setPixels: syncCurriedFunction(false, setPixels),
+    getPixels: syncCurriedFunction(false, getPixels),
+    flipH: syncCurriedFunction(false, flipH),
+    flipV: syncCurriedFunction(false, flipV),
+    setRotation: syncCurriedFunction(false, setRotation),
+    showMessage: syncCurriedFunction(false, showMessage),
+    flashMessage: syncCurriedFunction(false, flashMessage),
+    showLetter: syncCurriedFunction(false, showLetter),
+    loadImage: syncCurriedFunction(false, loadImage),
+    get rotation () { return rotation },
+    set rotation (angle) { setRotation(false, angle) },
+    get gamma () {},
+    set gamma (arr) {},
+    gammaReset: noop,
+    get lowLight () {},
+    set lowLight (bol) {},
     sync: {
-      setPixelsSync: setPixels,
-      setRotationSync: setRotation,
-      showLetterSync: showLetter
+      sleep,
+      clear: syncCurriedFunction(true, clear),
+      getPixel: syncCurriedFunction(true, getPixel),
+      setPixel: syncCurriedFunction(true, setPixel),
+      getPixels: syncCurriedFunction(true, getPixels),
+      setPixels: syncCurriedFunction(true, setPixels),
+      flipH: syncCurriedFunction(true, flipH),
+      flipV: syncCurriedFunction(true, flipV),
+      setRotation: syncCurriedFunction(true, setRotation),
+      showMessage: syncCurriedFunction(true, showMessage),
+      flashMessage: syncCurriedFunction(true, flashMessage),
+      showLetter: syncCurriedFunction(true, showLetter),
+      loadImage: syncCurriedFunction(true, loadImage),
+      get rotation () { return rotation },
+      set rotation (angle) { setRotation(true, angle, true) },
+      get gamma () {},
+      set gamma (arr) {},
+      gammaReset: noop,
+      get lowLight () {},
+      set lowLight (bol) {}
     }
   }
 }

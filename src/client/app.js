e182b16bdb686243c20485f94fd61a2ea15e8990
Sílvia Mur <pchiwan@gmail.com>
2018-11-09 00:04:27 +0100

    Fix handling of long keydown press on client

--- src/client/app.js
+++ src/client/app.js
@@ -6,6 +6,12 @@ import { isArrowKey } from './utils'
 
 const App = socket => {
   const handleKeyEvent = event => {
+    if (event.repeat) {
+      // prevent event from being fired repeteadly when the key is being held down
+      // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/repeat
+      return
+    }
+
     if (isArrowKey(event.keyCode)) {
       event.preventDefault()
     }
